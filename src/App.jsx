d789ddcc377b083bb0666eaa2a25c857dcb064a7
André Campos <roboreserva.andre@gmail.com>
2023-10-30 22:56:55 -0300

    subindo estrutura base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+export default function App() {
+  return(
+    <>
+      <div className="app">
+        <h1 className='app__titulo'>Essa é a base do meu projeto</h1>
+      </div>
+    </>
+  )
+}
